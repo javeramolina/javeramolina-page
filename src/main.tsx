c295eb62fcf34cf79a566ff45d367821df88681a
jesus vera <jesusvera@192.168.1.8>
2025-04-23 04:02:19 -0400

    fix deploy github

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css'
+import 'flag-icon-css/css/flag-icons.min.css';
 import App from './App.tsx'
+import './i18n'
 
 
 createRoot(document.getElementById('root')!).render(
