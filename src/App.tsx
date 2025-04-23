4f178fd2d53b49f4865c2b859b3ad0e754c7f72c
jesus vera <jesusvera@192.168.1.8>
2025-04-23 03:22:39 -0400

    remove code without use

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { useTranslation } from 'react-i18next';
+import Header from './components/Header.js';
+import Home from './components/Home.js';
 import About from './components/About.js';
 import Service from './components/Service.js';
-export default function App() {
 
+
+
+export default function App() {
+  const { t } = useTranslation();
 
   return (
     <div>
       
-      
+      <Header title={t('welcome')} />
+      <Home title={t('home')} />
       <Service></Service>
       <About/>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'flag-icon-css/css/flag-icons.min.css';
+
 import App from './App.tsx'
-import './i18n'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
