c8814b3ab2bb7707f614931ead54e22ccf8572a8
jesus vera <jesusvera@192.168.1.8>
2025-04-24 15:42:47 -0400

    clean page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,5 @@
-import project1 from "../assets/images/project-1.jpg";
 import project2 from "../assets/images/project-2.jpg";
 import project3 from "../assets/images/project-3.jpg";
-import project4 from "../assets/images/project-4.jpg";
 
 const Projects = () => {
 
